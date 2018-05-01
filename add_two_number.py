0b1f3f18a7d19c9bcc1de38062adaabaceb8233a
V <DigitalCrafts@ms-MacBook-Pro.localdomain>
2018-05-01 12:10:43 -0500

    Second Assignment initial commit

---
+++ add_two_number.py
@@ -0,0 +1,3 @@
+first_number = float(input("Please enter the first number: "))
+second_number = float(input("Please enter the second number: "))
+print(f"The total is: {first_number + second_number} ")
